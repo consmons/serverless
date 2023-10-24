a0f02ac8dc955c8a98ae06467b77c232bbeb02cf
consmons <coberg@berg-familie.de>
2023-10-24 22:59:04 +0200

    feat: Add dotenv relative path option

--- test/unit/lib/cli/conditionally-load-dotenv.test.js
+++ test/unit/lib/cli/conditionally-load-dotenv.test.js
@@ -4,6 +4,7 @@ const chai = require('chai');
 const path = require('path');
 const overrideEnv = require('process-utils/override-env');
 const fsp = require('fs').promises;
+const fse = require('fs-extra');
 const conditionallyLoadDotenv = require('../../../../lib/cli/conditionally-load-dotenv');
 
 chai.use(require('chai-as-promised'));
@@ -11,6 +12,7 @@ const expect = require('chai').expect;
 
 describe('test/unit/lib/cli/conditionally-load-dotenv.test.js', () => {
   let restoreEnv;
+  const relativeDotenvPath = 'test-path';
 
   before(async () => {
     const defaultFileContent = 'DEFAULT_ENV_VARIABLE=valuefromdefault';
@@ -19,6 +21,9 @@ describe('test/unit/lib/cli/conditionally-load-dotenv.test.js', () => {
     const stage = 'testing';
     const stageFileContent = 'STAGE_ENV_VARIABLE=valuefromstage';
     await fsp.writeFile(path.resolve(`.env.${stage}`), stageFileContent);
+
+    const relativeFileContent = 'RELATIVE_DOTENV_PATH_ENV_VARIABLE=valuefromrelativedotenvpath';
+    await fse.outputFile(path.resolve(relativeDotenvPath, '.env'), relativeFileContent);
   });
 
   beforeEach(() => {
@@ -33,11 +38,20 @@ describe('test/unit/lib/cli/conditionally-load-dotenv.test.js', () => {
 
     expect(process.env.DEFAULT_ENV_VARIABLE).to.equal('valuefromdefault');
     expect(process.env.STAGE_ENV_VARIABLE).to.be.undefined;
+    expect(process.env.RELATIVE_DOTENV_PATH_ENV_VARIABLE).to.be.undefined;
   });
 
   it('should load environment variables from stage .env file if matching stage', async () => {
     await conditionallyLoadDotenv({ stage: 'testing' }, { useDotenv: true });
     expect(process.env.DEFAULT_ENV_VARIABLE).to.be.undefined;
     expect(process.env.STAGE_ENV_VARIABLE).to.equal('valuefromstage');
+    expect(process.env.RELATIVE_DOTENV_PATH_ENV_VARIABLE).to.be.undefined;
+  });
+
+  it('should load environment variables using the relative path if provided', async () => {
+    await conditionallyLoadDotenv({}, { useDotenv: true, relativeDotenvPath });
+    expect(process.env.DEFAULT_ENV_VARIABLE).to.be.undefined;
+    expect(process.env.STAGE_ENV_VARIABLE).to.be.undefined;
+    expect(process.env.RELATIVE_DOTENV_PATH_ENV_VARIABLE).to.equal('valuefromrelativedotenvpath');
   });
 });
